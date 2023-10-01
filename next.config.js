52dfcfd8008a144f8875a9ca55b4e02b7951764d
tonioyeme <toniford125@icloud.com>
2023-10-01 11:39:35 -0400

    debug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/decentra-world-web',
-  assetPrefix: '/decentra-world-web/',
+  //basePath: '/decentra-world-web',
+  //assetPrefix: '/decentra-world-web/public/',
 }
 
 module.exports = nextConfig;
