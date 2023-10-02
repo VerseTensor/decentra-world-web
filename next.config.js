f00c19f3ed63f0f9ed2f4cde9327ece52e49c7d3
tonioyeme <toniford125@icloud.com>
2023-10-02 17:53:12 -0400

    debug

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/decentra-world-web/' : '',
+  assetPrefix: isProd ? '/decentra-world-web' : '',
   basePath: isProd ? '/decentra-world-web' : '',
 };
 
