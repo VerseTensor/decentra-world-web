64438f9c5fa4578536143367ec9f7ba8a35149f5
tonioyeme <toniford125@icloud.com>
2023-10-02 18:55:57 -0400

    debug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+console.log("isProd is: ", isProd);
+console.log("isProd in next.config.js: ", isProd);
+console.log("assetPrefix in next.config.js: ", isProd ? '/decentra-world-web' : '');
+
 
 const nextConfig = {
   reactStrictMode: true,
+  // assetPrefix: process.env.ASSET_PREFIX || '',
+  basePath: isProd ? '/decentra-world-web' : '',
 };
 
 module.exports = nextConfig;
