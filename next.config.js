7df4e822ae2ec7b9e243c1ae4af2ec92211feef0
tonioyeme <toniford125@icloud.com>
2023-10-02 18:15:47 -0400

    debug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+console.log("isProd is: ", isProd);
 
 const nextConfig = {
   reactStrictMode: true,
