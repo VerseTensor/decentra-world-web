76095367f26f2f1d37060e8811c2fdfbfd21a3c1
tonioyeme <tonioyeme@users.noreply.github.com>
2023-10-02 04:46:23 +0000

    deploy: 95c97dc48a9dac30049b03f759a3cfd0e2acc9bc

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  //basePath: '/decentra-world-web',
-  //assetPrefix: '/decentra-world-web/public/',
-}
-
-module.exports = nextConfig;
-
